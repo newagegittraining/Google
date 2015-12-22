fa1477270a8e89568d7b959fd05e83ff48b81280
sundar.inmaa <sundar.inmaa>
2015-12-22 11:12:39 +0530

    First changes - test

--- src/com/google/HomeServlet.java
+++ src/com/google/HomeServlet.java
@@ -8,6 +8,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * This is the home servlet class
+ * @author sundar.inmaa
+ *
+ */
 public class HomeServlet extends HttpServlet {
 	
 	@Override
